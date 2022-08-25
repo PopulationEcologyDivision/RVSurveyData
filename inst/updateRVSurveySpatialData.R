0c53616bf0945149eaf68ae7dd0fa2acde2785bd
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2022-08-25 15:52:52 +0000

    updates the nafo_sf layer

--- inst/updateRVSurveySpatialData.R
+++ inst/updateRVSurveySpatialData.R
@@ -7,12 +7,8 @@ updateRVSurveySpatialData <- function(){
   library(mapdata)
   library(sf)
   #stuff below is for Kasia's new improved layer.
-  # nafo_sf <- sf::st_read("inst/rawSpatial/NAFO_polyBlocks.shp")
-  # nafo_sf$Shape_Leng<- nafo_sf$ORIG_FID <- nafo_sf$Shape_Le_1 <- nafo_sf$Shape_Area <- nafo_sf$OBJECTID <- NULL
-  # colnames(nafo_sf)[colnames(nafo_sf)=="UnitArea"] <- "NAFO"
-  # nafo_sf$NAFO <- toupper(nafo_sf$NAFO)
-  # nafo_sf <- st_transform(nafo_sf, crs = 4326)
-  nafo_sf <- Mar.data::NAFOSubunits_sf
+  # the layer read in below already has the US areas, and new fields.
+  nafo_sf <- sf::st_read("c:/git/Maritimes/Mar.data/data-raw/NAFO/NAFO_BEST_KasiaUS2022.shp")
   usethis::use_data(nafo_sf, overwrite = TRUE)
   
   strataMar_sf <- sf::st_read("inst/rawSpatial/MaritimesRegionEcosystemAssessmentStrata(2014-).shp")
