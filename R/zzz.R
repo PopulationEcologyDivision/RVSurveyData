abd9a6454b511ca1aba3e196165fc120f810de3c
MMMcMahon <Mike.McMahon@dfo-mpo.gc.ca>
2022-11-17 18:16:14 +0000

    add draft note

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
 .onLoad <- function(libname, pkgname){
   updateCheck(gitPkg = 'PopulationEcologyDivision/RVSurveyData')
+  packageStartupMessage("This package is in draft form and may not reflect the actual data available")
 }
